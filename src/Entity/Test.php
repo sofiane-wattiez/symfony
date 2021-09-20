1184ccece81b3048719c067567cb1a0bc2ffd22d
sofiane-wattiez <sofiane.wattiez.developpeur@gmail.com>
2021-09-20 12:39:35 +0200

    test symfony function

---
+++ src/Entity/Test.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TestRepository::class)
+ */
+class Test
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $test2;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getTest2(): ?string
+    {
+        return $this->test2;
+    }
+
+    public function setTest2(string $test2): self
+    {
+        $this->test2 = $test2;
+
+        return $this;
+    }
+}
